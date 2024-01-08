7dd7fe130f01ce76f9742b6cd2330db71b1b2e90
dev-relick <dev.relick@gmail.com>
2024-01-08 11:42:55 +0900

    Add launch configuration and dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,71 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'path';
-
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+import { app, BrowserWindow, Tray, Menu, globalShortcut } from "electron";
+import path from "path";
+import { is } from "@electron-toolkit/utils";
+//import ffi from 'ffi-napi';
+import { Library } from "ffi-napi";
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
   });
-
-  // and load the index.html of the app.
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+  console.log(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  console.log(__dirname);
+  if (is.dev) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    );
   }
 
-  // Open the DevTools.
   mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+/*app.on("ready", createWindow);
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
-});
+});*/
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+let tray: Tray = null;
+app.whenReady().then(() => {
+  const iconPath: string = path.join(__dirname,`application-icon.png`);
+  tray = new Tray(iconPath);
+  const contextMenu = Menu.buildFromTemplate([
+    { label: "Item1", type: "radio" },
+    { label: "Item2", type: "radio" },
+    { type: "separator" },
+    { label: "Other" },
+  ]);
+  tray.setToolTip("This is my application.");
+  
+  const user32 = new Library('user32.dll', {
+   /* 'FindWindowW': ['int', ['string', 'string']],
+    'GetForegroundWindow': ['long', []],
+    'ShowWindow': ['int', ['int', 'int']]*/
+  });
+
+  /*globalShortcut.register("CommandOrControl+Q", () => { console.log("CommandOrControl+Q is pressed");
+  const forecusedWindow = user32.GetForegroundWindow();
+  console.log("CommandOrControl+Q is pressed");
+  console.log("Focused Application:", forecusedWindow);
+});*/
+
+  tray.setContextMenu(contextMenu);
+});
